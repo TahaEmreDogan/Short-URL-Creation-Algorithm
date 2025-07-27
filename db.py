98d5b8e4bc4d44e51868dd56c2eaa7ec80321566
TahaEmreDogan <tahaemredogan@hotmail.com>
2025-07-27 23:40:57 +0300

    URL shortening added

--- db.py
+++ db.py
@@ -1,5 +1,8 @@
 import psycopg2
 from psycopg2 import Error
+import random
+import string
+
 
 def db_connect():
     return psycopg2.connect(
@@ -29,16 +32,16 @@ def user_register(user_name,user_password,register_date):
         if cursor : cursor.close()
         if connection : connection.close()
 
-def user_login(user_name, user_password, login_date):
+def user_login(user_name, user_password):
     connection = None
     cursor = None
     try:
         connection = db_connect()
         cursor = connection.cursor()
         query = """
-            SELECT user_name, user_password, login_date) VALUES(%s,%s,%s)
+            SELECT (user_name, user_password) FROM users VALUES(%s,%s)
         """
-        cursor.execute(query, (user_name, user_password, login_date))
+        cursor.execute(query, (user_name, user_password))
         connection.commit()
         return True
     except (Exception, Error) as error:
@@ -47,3 +50,28 @@ def user_login(user_name, user_password, login_date):
     finally:
         if cursor: cursor.close()
         if connection: connection.close()
+
+
+def shorten_links(original_url, shortened_url):
+    characters = string.ascii_letters + string.digits
+    domain = 'https://short.url/'
+    length = 6
+    shortened_url = domain + ''.join(random.choices(characters, k=length))
+    connection = None
+    cursor = None
+    try:
+        connection = db_connect()
+        cursor = connection.cursor()
+        query = """
+            INSERT INTO url (original_url, shortened_url) VALUES(%s,%s)
+        """ 
+        cursor.execute(query, (original_url, shortened_url))
+        connection.commit()
+        return shortened_url
+    except (Exception, Error) as error:
+        print("Database Error:",error)
+        return False
+    finally:
+        if cursor: cursor.close()
+        if connection: connection.close()
+
---
+++ url.py
@@ -0,0 +1,5 @@
+class URL():
+    def __init__(self, id, original_url, shortened_url):
+        self.id = id
+        self.original_url = original_url
+        self.shortened_url = shortened_url
